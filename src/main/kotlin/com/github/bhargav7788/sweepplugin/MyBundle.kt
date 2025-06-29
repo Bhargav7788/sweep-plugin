10197eaa97b7516cabc352b4dbbd5658090e9954
GitHub Action <action@github.com>
2025-06-29 01:19:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/bhargav7788/sweepplugin/MyBundle.kt
+++ src/main/kotlin/com/github/bhargav7788/sweepplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bhargav7788.sweepplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bhargav7788/sweepplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bhargav7788/sweepplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.bhargav7788.sweepplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bhargav7788.sweepplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
